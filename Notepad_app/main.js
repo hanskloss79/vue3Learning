d85801b54b88fbdc0b1ee7867dc43b7f99777376
hanskloss79 <piohandz79@gmail.com>
2023-03-16 16:52:39 +0100

    inputComponent created, events handlers added, displaying the list of submitted notes with timestamps

--- Notepad_app/main.js
+++ Notepad_app/main.js
@@ -1,14 +1,46 @@
 // znaczniki w templacie na stronę HTML
 const inputComponent = {
-    template: `<input class="input is-small" type="text" />`
-}
+    template: `<input :placeholder="placeholder" v-model="input" 
+            v-on:keyup.enter="monitorEnterKey" class="input is-small" type="text" />`,
+    props: ["placeholder"],
+    emits: ["add-note"],
+    data() {
+        return {
+            input: "",
+        };
+    },
+    methods: {
+        monitorEnterKey() {
+            this.$emit("add-note", {
+                note: this.input,
+                timestamp: new Date().toLocaleString(),
+            });
+            this.input = "";
+        },
+    },
+};
 
-// komponent Vue - przypisanie do znacznika HTML
 const notepad = {
+    data() {
+        return {
+            notes: [],
+            timestamps: [],
+            placeholder: 'Wprowadź notatkę'
+        }
+    },
+    // komponent Vue - przypisanie do znacznika HTML
     components: {
         'input-component': inputComponent
+    },
+
+    methods: {
+        // dodanie notatki do listy
+        addNote(event){
+            this.notes.push(event.note);
+            this.timestamps.push(event.timestamp);
+        }
     }
-}
+};
 
 Vue.createApp(notepad).mount('#notepad');
 

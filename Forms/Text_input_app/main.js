ca6b34074ec796d191d0a736a6697156600d9211
hanskloss79 <piohandz79@gmail.com>
2023-03-21 09:14:07 +0100

    InputForm component and event handler created. List of items displays below the form.

--- Forms/Text_input_app/main.js
+++ Forms/Text_input_app/main.js
@@ -1,17 +1,34 @@
-const InputForm = { 
+/* Notice the form element and accompanying list markup are wrapped within a single div
+element. With Vue template declarations, it’s a must to wrap templates in a single root
+element. */
+
+const InputForm = {
   template: `
     <div class="input-form">
       <form @submit="submitForm" class="ui form">
         <div class="field">
-          <input ref="newItem" type="text" placeholder="Dodaj element garderoby" />
+          <input v-model="newItem" type="text" placeholder="Dodaj element garderoby" />
         </div>
         <button class="ui button">Wyślij</button>
       </form>
+      <div class="ui segment">
+        <h4 class="ui header">Odzież</h4>
+        <ul>
+          <li v-for="item in items" class="item">{{ item }}</li>
+        </ul>
+      </div>
     </div>`,
+  data() {
+    return {
+      newItem: '',
+      items: []
+    }
+  },
   methods: {
     submitForm(evt) {
+      this.items.push(this.newItem);
+      newItem = '';
       evt.preventDefault();
-      console.log(this.$refs.newItem.value)
     }
   }
 }

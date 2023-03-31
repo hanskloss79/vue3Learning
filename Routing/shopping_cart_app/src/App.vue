9fd0be9d6ddaac4434352f8355448a791ddb129a
hanskloss79 <piohandz79@gmail.com>
2023-03-31 09:53:05 +0200

    Initial commit for shopping_cart_app - routing in Vue 3 by using vue-router.

---
+++ Routing/shopping_cart_app/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-4">
+          <CartList />
+        </div>
+        <div class="column is-8">
+          <ProductList />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CartList from './components/cart/CartList';
+import ProductList from './components/product/ProductList';
+
+export default {
+  name: 'App',
+  components: {
+    CartList,
+    ProductList
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  background: #f2f6fa;
+}
+
+#app {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media screen and (min-width: 769px) {
+  html,
+  body {
+    height: 100vh;
+  }
+}
+</style>
---
+++ Routing/shopping_cart_app/src/components/product/ProductListItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2 class="has-text-weight-bold">{{ productItem.title }}
+            <span v-on:click="addCartItem(productItem)"
+                class="tag is-primary is-pulled-right has-text-white">
+                Dodaj do koszyka
+            </span>
+        </h2>
+        <p>{{ productItem.description }}</p>
+        <span class="has-text-primary has-text-weight-bold">
+             {{ productItem.price }} zł
+        </span>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+    name: 'ProductListItem',
+    props: ['productItem'],
+
+    methods: {
+        ...mapActions([
+            'addCartItem'
+        ])
+        
+    },
+
+}
+</script>
+<style scoped>
+.tag {
+    cursor: pointer;
+}
+</style>

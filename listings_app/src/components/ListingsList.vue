c8822d162784792a0a1388570929e75dce1da820
hanskloss79 <piohandz79@gmail.com>
2023-04-08 22:02:27 +0200

    Initial commit for listings_app, prepared for updating to using composition API.

---
+++ listings_app/src/components/ListingsList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="listings">
+    <Notification :notification="notification" :isDark="isDark" />
+    <div v-for="listing in listings" :key="listing.id">
+      <ListingsListItem :listing="listing" :isDark="isDark" />
+    </div>
+    <button
+      class="button is-light"
+      :class="{ 'is-primary': isDark, 'is-info': !isDark }"
+      @click="resetListings" 
+      :disabled="listings.length === 3">
+      Reset
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import ListingsListItem from './ListingsListItem';
+import ListingsNotification from './ListingsNotification';
+
+export default {
+  name: 'ListingsList',
+  props: ['listings', 'isDark'],
+  data() {
+    return {
+      notification: null,
+    }
+  },
+  methods: {
+    ...mapActions([
+      'resetListings'
+    ])
+  },
+  components: {
+    ListingsListItem,
+    // eslint-disable-next-line
+    ListingsNotification
+  },
+  mounted() {
+    this.notification = "Welcome to NewlineBnB!";
+
+    setTimeout(() => {
+      this.notification = null;
+    }, 1000);
+  }
+}
+</script>
